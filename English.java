826cfe654e6da45f4f59ec7d33b9f764c8dd6552
Kerwin Chen <kchen37@stuy.edu>
2018-01-08 07:33:19 -0500

    added English cucumber Subclass

---
+++ English.java
@@ -0,0 +1,14 @@
+public class English extends Cucumber
+{
+    //Constructor
+    public English()
+    {
+	super();
+	name = "Eng";
+	nutrition = 5;
+	ripeDuration = 2;
+    }
+    public String about()
+    {return "";}
+}
+	
