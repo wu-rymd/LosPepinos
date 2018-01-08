1259115c1a6dbf3a6e78ca60cf62d729918524b6
Kerwin Chen <kchen37@stuy.edu>
2018-01-08 07:35:31 -0500

    added Persian Cucumber Subclass

---
+++ Persian.java
@@ -0,0 +1,14 @@
+public class Persian extends Cucumber
+{
+    //Constructor
+    public Persian()
+    {
+	super();
+	name = "Per";
+	nutrition = 20;
+	ripeDuration = 5;
+    }
+    public String about()
+    {return "";}
+}
+	
