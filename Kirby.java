672c16ff162f07f11a187d242787a90c6cd52501
Kerwin Chen <kchen37@stuy.edu>
2018-01-08 07:34:05 -0500

    added Kirby Cucumber Subclass

---
+++ Kirby.java
@@ -0,0 +1,14 @@
+public class Kirby extends Cucumber
+{
+    //Constructor
+    public Kirby()
+    {
+	super();
+	name = "Kir";
+	nutrition = 20;
+	ripeDuration = 5;
+    }
+    public String about()
+    {return "";}
+}
+	
