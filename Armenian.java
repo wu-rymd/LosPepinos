72c8f6207dbb3321a619597c09bbc0d0ecc7a40b
Kerwin Chen <kchen37@stuy.edu>
2018-01-08 07:31:56 -0500

    Added Armenian Cucumber Subclass

---
+++ Armenian.java
@@ -0,0 +1,14 @@
+public class Armenian extends Cucumber
+{
+    //Constructor
+    public Armenian()
+    {
+	super();
+	name = "Arm";
+	nutrition = 20;
+	ripeDuration = 5;
+    }
+    public String about()
+    {return "";}
+}
+	
