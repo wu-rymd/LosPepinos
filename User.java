bec00f79864692fa2d4f08ecc085da6fed07106d
Kerwin Chen <kchen37@stuy.edu>
2018-01-09 07:33:44 -0500

    added User class

---
+++ User.java
@@ -0,0 +1,21 @@
+public class User
+{
+    private int money;
+    private String name;
+    
+    public User()
+    {
+	name = "";
+	money = 30;
+    }
+    public User(String n)
+    {
+	this;
+	name = n;
+    }
+
+    public void plant(int xcor, int ycor, int selection)
+    {
+	if (selection == 1){
+	    English = new English(); 
+}
