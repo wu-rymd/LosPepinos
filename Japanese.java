2c08b34fc74493cbb69c19872550ec60331d7597
Kerwin Chen <kchen37@stuy.edu>
2018-01-08 20:39:20 -0500

    added Japanese Cucumber Subclass

---
+++ Japanese.java
@@ -0,0 +1,16 @@
+public class Japanese extends Cucumber
+{
+    //Constructor
+
+    public Japanese()
+    {
+	super();
+	name = "Jpn";
+	nutrition = 25;
+	ripeDuration = 6;
+    }
+    
+    public String about()
+    {return "";}
+
+}
