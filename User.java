cf4230d433474a5ae07f324f5989b59ba1425b85
Kerwin Chen <kchen37@stuy.edu>
2018-01-10 07:34:49 -0500

    added Woo.java(Skateboard)

--- User.java
+++ User.java
@@ -3,6 +3,7 @@ public class User
     private int money;
     private String name;
     
+    
     public User()
     {
 	name = "";
@@ -10,12 +11,40 @@ public class User
     }
     public User(String n)
     {
-	this;
+	this();
 	name = n;
     }
 
     public void plant(int xcor, int ycor, int selection)
     {
-	if (selection == 1){
-	    English = new English(); 
+	String Sselection = ""+selection;
+	if (Sselection == "1"){
+	    English eng = new English();
+	    Farm.plant(xcor, ycor, eng);
+	}
+	else if (Sselection == "2"){
+	    Kirby kir = new Kirby();
+	    Farm.plant(xcor, ycor, kir);
+	}
+
+	else if (Sselection == "3"){
+	    Persian per = new Persian();
+	    Farm.plant(xcor, ycor, per);
+	}
+
+	else if (Sselection == "4"){
+	    Armenian arm = new Armenian();
+	    Farm.plant(xcor, ycor, arm);
+	}
+
+	else if (Sselection == "5"){
+	    Japanese jpn = new Japanese();
+	    Farm.plant(xcor, ycor, jpn);
+	}
+	    
+    }
+    public static void main(String[] args){
+	User lim = new User();
+	System.out.println(lim.plant(1,2,1));
+    }
 }
---
+++ Woo.java
@@ -0,0 +1,4 @@
+public class Woo
+{
+    public static void main(String[] args){}
+}
