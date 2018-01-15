3e4eaca21b00d974b0d413aa1fb3c2c68e3559b1
jerry1ye10 <jerry1ye10@gmail.com>
2018-01-15 13:41:20 -0500

    updated Farm.java with constructor and added comments

--- Farm.java
+++ Farm.java
@@ -1,9 +1,15 @@
 public class Farm extends Matrix{
-  public Object getCucumber(int n, int n2){
-    super.get(n,n2);
+  public Farm(){//automatically generates a 2 row, 2 column matrix as a farm
+    super();
   }
-  public void setCucumber(int n, int n2, Object O){
-    super.set(n, n2, O);
+  public Farm(int n){//generates a n row n column matrix as a farm
+    super(n);
+  }
+  public Object getCucumber(int r, int c){ //calls on Matrix's get method to implement GetCucumber
+    super.get(r,c);
+  }
+  public void setCucumber(int r, int c, Object O){//calls on Matrix's set method to implement setCucumber
+    super.set(r, c, O);
   }
 
 }
