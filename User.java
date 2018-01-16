7eb8e7ee6bb871730d7a2216898f01a74fe327e5
Raymond Wu <rwu6@stuy.edu>
2018-01-16 00:19:15 -0500

    Cleanup/remove unnecessary code

--- User.java
+++ User.java
@@ -1,19 +1,12 @@
 public class User
 {
     private int money;
-    private String name;
     
     
     public User()
     {
 	name = "";
 	money = 50;
-    }
-    
-    public User(String n)
-    {
-	this();
-	name = n;
     }
 
     public int getMoney(){
@@ -22,13 +15,5 @@ public class User
     
     public void setMoney(int n){
 	money = n;
-    }
-    
-    public String getName(){
-	return name;
-    }
-    
-    public void setName(String s){
-	name = s; 
     }
 }
